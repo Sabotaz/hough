2a70c57872903578185a6a9bc1a4ce5cc55e69c6
sablier <sablier@zendikar.fr>
2017-02-09 18:20:40 +0100

    use pcl as module

--- Android.mk
+++ Android.mk
@@ -7,12 +7,12 @@ LOCAL_SRC_FILES = hough.cpp
 LOCAL_CPPFLAGS    := -std=c++11 -D_REENTRANT -fexceptions -frtti -w -Wall
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH) \
-                    $(LOCAL_PATH)/../eigen/eigen3/build/include/
+                    $(LOCAL_PATH)/../eigen/eigen3/build/include/ \
 
 LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)
 LOCAL_EXPORT_LDLIBS := -lz
 
 LOCAL_STATIC_LIBRARIES := Eigen \
-                          libpcl_common
+                          PCL
 
 include $(BUILD_STATIC_LIBRARY)
